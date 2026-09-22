96b2b9feec4bb3e996d378cf64ba1cd398b9636a
Justin Holmes <jholmes@gamecreekvideo.com>
2026-09-22 11:40:13 -0400

    Cleaned up some left over debugging

--- plugins/modules/subnet.py
+++ plugins/modules/subnet.py
@@ -289,8 +289,6 @@ def ensure_present(module, client: Client):
 
     # find a match on server, if none, create new object
     items = client.get(ENDPOINT).json
-    # item = get_match(items, "name", module.params["name"])
-    # print(item)
     item = get_complex_match(
         items,
         {"name": module.params["name"], ("vlan", "fabric"): module.params["fabric"]},
